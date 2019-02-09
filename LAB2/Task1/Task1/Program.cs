7d765c4690638e84c2c3ddef2b31e0b87e5750ec
Merlan <merlan01tel@gmail.com>
2019-02-09 19:52:30 +0600

    init commit

---
+++ LAB2/Task1/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            s = Console.ReadLine();
+            string t;
+            t = s;
+
+            char[] arr = t.ToCharArray();
+            Array.Reverse(arr);
+
+            string m = new string(arr);
+
+            if (s == m  )
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
---
+++ LAB2/Task2/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = new FileStream(@"C:\Users\Merlan\Desktop\LABS\READ.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fileStream);
+
+            String line = sr.ReadLine();
+
+            String[] stringNumbers = line.Split(" ");
+
+            int sum = 0;
+
+            for (int i = 0; i < stringNumbers.Length; ++i)
+            {
+                int num = int.Parse(stringNumbers[i]);
+                sum += num;
+            }
+
+            Console.WriteLine(sum);
+
+
+
+            sr.Close();
+            fileStream.Close();
+        }
+    }
+}
